4aae6d79ff0779e5ec4f4b2d4d5312f60a838f50
Ylian1512 <yliansarra002@gmail.com>
2024-06-20 21:05:03 +0200

    ....

---
+++ script.js
@@ -0,0 +1,41 @@
+document.addEventListener('DOMContentLoaded', () => {
+    loadCookies()
+
+    ///////////////////////////////////////////////////////////////////////
+
+});
+
+
+// Functions
+// Select Menus
+function app_changeTheme() {
+    const themeSelector = document.getElementById("sm-themeSelector");
+    const selectedTheme = themeSelector.value;
+    const themeLink = document.getElementById("theme");
+    themeLink.href = selectedTheme;
+    console.log(`[LOG] Thème changé en ${selectedTheme}`)
+    setCookie('theme', selectedTheme, 365)
+}
+
+// Cookies
+function loadCookies() {
+    if (!getCookie("theme")) {
+        setCookie("theme", "light.css", 365 * 4)
+    }
+}
+function setCookie(name, value, days) {
+    const date = new Date();
+    date.setTime(date.getTime() + (days * 86400000));
+    const expires = "expires=" + date.toUTCString();
+    document.cookie = name + "=" + value + ";" + expires + ";path=/";
+}
+function getCookie(name) {
+    const nameEQ = name + "=";
+    const ca = document.cookie.split(';');
+    for (let i = 0; i < ca.length; i++) {
+        let c = ca[i];
+        while (c.charAt(0) == ' ') c = c.substring(1, c.length);
+        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
+    }
+    return null;
+}
